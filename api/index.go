fdc7371cc47118496ded702a7fd296e18f07f4a2
bharatundef <bharat.saini@undef.studio>
2024-04-30 21:04:07 +0530

    added api package with simple Hello World return path /hello

--- api/index.go
+++ api/index.go
@@ -1,16 +1,26 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-func HelloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Hello from Vercel!"})
+var(
+	app *gin.Engine
+)
+
+func myRoute(r *gin.RouterGroup){
+	r.GET("/hello", func(c *gin.Context){
+		c.String(http.StatusOK,"Hello World")
+	})
 }
 
-func SetupRouter(r *gin.Engine) {
-	r.GET("/hello", HelloHandler)
+func init(){
+	app = gin.New()
+	r := app.Group("/api")
+	myRoute(r)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request){
+	app.ServeHTTP(w,r)
 }
